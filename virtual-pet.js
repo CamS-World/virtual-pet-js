f5ad31971a96b822c1d1b9c690630e445524171a
CamS-World <cstephen13@mail.bw.edu>
2022-01-09 13:51:37 -0500

    Alan types so fast

--- virtual-pet.js
+++ virtual-pet.js
@@ -1,1 +1,20 @@
+class VirtualPet {
+  constructor(name, desc, hunger, thirst) {
+    this.name = name;
+    this.desc = desc;
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
 
+  feed() {
+    this.hunger -= 5;
+    this.thirst += 10;
+  }
+
+  watered() {
+    this.thirst -= 5;
+    this.hunger += 10;
+  }
+}
+
+export default VirtualPet;
--- virtual-pet.test.js
+++ virtual-pet.test.js
@@ -1,1 +1,32 @@
+import VirtualPet from "./virtual-pet";
 
+//test go inbetween curlys
+describe("VirtualPet", () => {
+  test("does it return a name", () => {
+    const underTest = new VirtualPet("Drake");
+    expect(typeof underTest.name).toMatch("string");
+  });
+
+  test("does it return Drake", () => {
+    const underTest = new VirtualPet("Drake");
+    expect(underTest.name).toEqual("Drake");
+  });
+
+  test("does it return hunger of 45 when fed", () => {
+    const underTest = new VirtualPet("Drake", "dog", 50, 50);
+    underTest.feed();
+    expect(typeof underTest.hunger).toMatch("number");
+  });
+
+  test("does it return hunger of 45 when fed", () => {
+    const underTest = new VirtualPet("Drake", "dog", 50, 50);
+    underTest.feed();
+    expect(underTest.hunger).toEqual(45);
+  });
+
+  test("does it return thirst of 45 when watered", () => {
+    const underTest = new VirtualPet("Drake", "dog", 50, 50);
+    underTest.watered();
+    expect(underTest.thirst).toEqual(45);
+  });
+});
